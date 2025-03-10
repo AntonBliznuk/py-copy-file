a750b6479796afc5f9c12bedb71d73b636329e67
Anton Bliznuk <antonbliznuk71@gmail.com>
2025-03-10 12:22:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+def command_validation(command: str) -> None:
+    if len(command.split(" ")) != 3:
+        raise ValueError
+    if "cp" not in command:
+        raise ValueError
+    if command.split(" ")[1] == command.split(" ")[2]:
+        raise ValueError
+    with open(command.split(" ")[1]):
+        pass
+
+
+def copy_file(command: str) -> None:
+    try:
+        command_validation(command)
+    except Exception:
+        return
+
+    origin_file_name = command.split(" ")[1]
+    copy_file_name = command.split(" ")[2]
+
+    with open(origin_file_name, "r") as origin, open(copy_file_name, "w") as copy_file:  # noqa: E501
+        content = origin.read()
+        copy_file.write(content)
